14e36e5b06b64c02932b84bbe4c69cff7f46199d
ItsRhetorical <ItsRhetorical@users.noreply.github.com>
2017-06-16 15:13:13 -0500

    First Commit
    Basic Procject and scene with tracking graph structure of user clicks. and UI elements to visualize the structure

---
+++ Assets/Scripts/Connections.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Connections : MonoBehaviour {
+
+    public List<GameObject> NodeList = new List<GameObject>();
+    public List<Vector3> ConnectionEndpoints = new List<Vector3>();
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        DrawConnections();
+    }
+
+    public void DrawConnections()
+    {
+            ConnectionEndpoints.Clear();
+
+        if (name == "Preview Tower")
+        {
+            NodeList.Clear();
+            FindNearbyTowers(true);
+        }
+            
+            foreach (GameObject node in NodeList)
+            {
+                //we have to bounce the line back and forth to look like the lines originate from the object
+                ConnectionEndpoints.Add(transform.position); //add self
+                ConnectionEndpoints.Add(node.transform.position); //add next node in list
+            }
+
+           // print("I am " + name + ", Connected to " + NodeList.Count + " Showing connections " + (ConnectionEndpoints.Count / 2));
+
+            GetComponent<LineRenderer>().positionCount = ConnectionEndpoints.Count;
+            GetComponent<LineRenderer>().SetPositions(ConnectionEndpoints.ToArray());
+            GetComponent<LineRenderer>().sortingLayerName = "Player";
+        
+    }
+
+    public void FindNearbyTowers(bool previewMode)
+    {
+        var PlayerInterface = GameObject.Find("Main Camera").GetComponent<PlayerInterface>();
+        foreach (GameObject node in PlayerInterface.allNodesList)
+        {
+            //print("Distance to " + node.name + " is " + Vector3.Distance(newTowerObj.transform.position, node.transform.position));
+            if (Vector3.Distance(transform.position, node.transform.position) <= PlayerInterface.TowerConnectionDistance)
+            {
+                NodeList.Add(node);
+                if (!previewMode)
+                    node.GetComponent<Connections>().NodeList.Add(this.gameObject);
+
+                //    print("Connecting:"+node.name+node.transform.position.ToString()+" and "+newTowerObj.name + newTowerObj.transform.position.ToString());
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInterface.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInterface : MonoBehaviour {
+
+    public GameObject Tower;
+    public double TowerConnectionDistance;
+    public List<GameObject> allNodesList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+
+        GameObject previewTower = Instantiate(Tower, new Vector2(0,0), Quaternion.identity) as GameObject;
+        previewTower.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .2f);
+        previewTower.name = "Preview Tower";
+        previewTower.tag = "Preview";
+        previewTower.SetActive(true);
+
+        BuildTower(Tower, new Vector2(0, 0));
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            BuildTower(Tower,Get2DMousePosition());
+        }
+        GameObject previewTower = GameObject.Find("Preview Tower");
+            previewTower.transform.position = Get2DMousePosition();
+    }
+
+    public Vector2 Get2DMousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.orthographicSize;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 mousePos2d = new Vector2(mousePos.x, mousePos.y);
+        //print(mousePos.x);
+        //print(mousePos.y);
+        return mousePos2d;
+    }
+
+    public void BuildTower(GameObject Tower, Vector2 MousePos)
+    {
+        allNodesList.Clear();
+        allNodesList.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("Tower")));
+
+        GameObject newTowerObj = Instantiate(Tower, MousePos, Quaternion.identity) as GameObject;
+
+        newTowerObj.GetComponent<Connections>().FindNearbyTowers(false);
+
+        newTowerObj.name = "Tower (" + allNodesList.Count + ")";
+        newTowerObj.SetActive(true);
+
+        allNodesList.Add(newTowerObj);
+        
+    }
+
+
+}
