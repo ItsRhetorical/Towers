22200709844ee04fe09547b37c36c77a73618160
ItsRhetorical <ItsRhetorical@users.noreply.github.com>
2017-06-16 15:18:41 -0500

    Unity Files

---
+++ oldscripts/Clicking.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Clicking : MonoBehaviour {
+
+    public GameObject HomeBase;
+    public GameObject Tower;
+    public Graph<GameObject> towerGraph = new Graph<GameObject>();
+    
+    // Use this for initialization
+    void Start (){
+        GraphNode<GameObject> BaseNode = new GraphNode<GameObject>(HomeBase);
+        towerGraph.Add(BaseNode);
+        print(towerGraph[0].Data.transform.position.x.ToString());
+        print(towerGraph[0].Data.transform.position.y.ToString());
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+		if (Input.GetMouseButtonDown (0)) 
+		{
+
+            PlaceTower(Get2DMousePosition());
+
+        }
+		if (Input.GetMouseButtonDown (1)) 
+		{
+			//print ("Right Clicked!");
+		}
+		if (Input.GetMouseButtonDown (2)) 
+		{
+            var x = 1;
+			//print ("Center Clicked!");
+		}
+	}
+
+    public Vector2 Get2DMousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.orthographicSize;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 mousePos2d = new Vector2(mousePos.x, mousePos.y);
+        //print(mousePos.x);
+        //print(mousePos.y);
+        return mousePos2d;
+    }
+
+    public void PlaceTower(Vector2 position)
+    {
+        //list of other towers
+        GameObject[] Towers = GameObject.FindGameObjectsWithTag("Tower");
+
+        //add new tower GameObject
+        GameObject newTowerObj = Instantiate(Tower, Get2DMousePosition(), Quaternion.identity) as GameObject;
+        //add our object to the graph
+        GraphNode<GameObject> towerObjNode = new GraphNode<GameObject>(newTowerObj);
+        // rename it to match/be unique
+        towerObjNode.Data.name = "Tower (" + (towerGraph.Count - 1) + ")";
+
+        //look through a list of other towers and find ones within 1
+        foreach (GameObject obj in Towers)
+        {
+            if (Vector3.Distance(obj.transform.position,newTowerObj.transform.position) >= 1)
+            {
+                towerGraph.AddUndirectedEdge(towerGraph.Find, towerObjNode);
+            }
+        }
+        
+
+
+        
+    }
+}
---
+++ oldscripts/Graph.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Hello World!");
+//        NodeList<string> nl = new NodeList<string>();
+//        GraphNode<string> node1 = new GraphNode<string>();
+//        GraphNode<string> node2 = new GraphNode<string>();
+//        node1.Data = "First Node";
+//        node2.Data = "Second Node";
+//                nl.Add(node2);
+
+//                Node<string> secondNodeFindObject = nl.Fin
+//                dByValue("Second Node");
+
+//                node1.Connections = nl;
+//                Console.WriteLine(node1.Data);
+//                Console.WriteLine(secondNodeFindObject.Data);
+
+//                Console.WriteLine(node1.Connections[0].Data);
+        
+//        Graph<string> g = new Graph<string>();
+//        g.AddNode(node1);
+//        g.AddNode(node2);
+//        GraphNode<string> node3 = g.AddNode("Node Three");
+//        g.AddDirectedEdge(node1, node2);
+//        g.AddUndirectedEdge(node2, node1);
+
+//        Console.WriteLine(g.Nodes);
+        
+//    }
+//}
+
+public class Node<T>
+{
+    public T Data { get; set; }
+    public NodeList<T> Connections = new NodeList<T>();
+
+    public Node() { }
+    public Node(T data) : this(data, null) { }
+    public Node(T data, NodeList<T> connections)
+    {
+        this.Data = data;
+        if (connections == null)
+            this.Connections = new NodeList<T>();
+        else
+            this.Connections = connections;
+    }
+
+}
+
+public class NodeList<T> : List<Node<T>>
+{
+    public NodeList() : base() {}
+    public NodeList(Node<T> node)
+    {
+        base.Add(node);
+    }
+
+    public Node<T> FindByValue(T value)
+    {
+        var result = FindAll(item => item != null && item.Data.Equals(value));
+
+        if (result.Count == 0)
+            return null;
+        return result[0];
+    }
+
+}
+
+public class GraphNode<T> : Node<T>
+{
+    private List<int> costs;
+    public GraphNode() : base() { }
+    public GraphNode(T data) : base(data) { }
+    public GraphNode(T data, NodeList<T> connections) : base(data, connections) { }
+}
+
+public class Graph<T> : List<GraphNode<T>>
+{
+    private NodeList<T> nodeSet;
+
+    public Graph() : this(null) { }
+    public Graph(NodeList<T> nodeSet)
+    {
+        if (nodeSet == null)
+            this.nodeSet = new NodeList<T>();
+        else
+            this.nodeSet = nodeSet;
+    }
+
+    public void AddNode(GraphNode<T> node)
+    {
+        //add node that exists
+        nodeSet.Add(node);
+        //return node;
+    }
+    public void AddNode(Node<T> node)
+    {
+        //add node that exists
+        nodeSet.Add(node);
+        //return node;
+    }
+    public GraphNode<T> AddNode(T data)
+    {
+        //create node with only value
+        GraphNode<T> node = new GraphNode<T>(data);
+        nodeSet.Add(node);
+        return node;
+    }
+    public void AddDirectedEdge(GraphNode<T> from, GraphNode<T> to)
+    {
+        from.Connections.Add(to);
+    }
+    public void AddUndirectedEdge(GraphNode<T> from, GraphNode<T> to)
+    {
+        from.Connections.Add(to);
+        to.Connections.Add(from);
+    }
+
+    /**
+        new public bool Contains(T value)
+        {
+            //contains by node data "name"
+            return nodeSet.FindByValue(value) != null;
+        }
+        new public int Count
+        {
+            get { return nodeSet.Count; }
+        }
+    **/
+}
